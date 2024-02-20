ed4cc9b8ebc0f0ca5eda113468e5e988dbedb8cc
LeeJungBum <jungbum05114@gmail.com>
2024-02-20 10:18:37 +0900

    feat: UsedException 추가

---
+++ src/main/java/com/nhnacademy/inkbridge/backend/exception/UsedException.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.inkbridge.backend.exception;
+
+/**
+ * class: UsedException.
+ *
+ * @author JBum
+ * @version 2024/02/19
+ */
+public class UsedException extends RuntimeException {
+
+    public UsedException(String message) {
+        super(message);
+    }
+
+}
