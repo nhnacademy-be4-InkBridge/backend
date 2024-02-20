b54e98a033a91365ce451f63d5ef61840943acc8
LeeJungBum <jungbum05114@gmail.com>
2024-02-20 10:19:07 +0900

    feat: repository 추가

--- src/main/java/com/nhnacademy/inkbridge/backend/repository/MemberCouponRepository.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/repository/MemberCouponRepository.java
@@ -3,7 +3,6 @@ package com.nhnacademy.inkbridge.backend.repository;
 import com.nhnacademy.inkbridge.backend.entity.Coupon;
 import com.nhnacademy.inkbridge.backend.entity.Member;
 import com.nhnacademy.inkbridge.backend.entity.MemberCoupon;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -14,6 +13,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface MemberCouponRepository extends JpaRepository<MemberCoupon,String> {
 
+    /**
+     * 특정 쿠폰이 특정 회원에게 이미 발급되었는지 확인합니다.
+     *
+     * @param coupon 쿠폰 발급 여부를 확인할 쿠폰
+     * @param member 쿠폰 발급 여부를 확인할 회원
+     * @return 해당 회원이 해당 쿠폰을 이미 발급받았으면 true, 그렇지 않으면 false를 반환합니다.
+     */
     boolean existsByCouponAndMemberAnd(Coupon coupon, Member member);
 
 }
