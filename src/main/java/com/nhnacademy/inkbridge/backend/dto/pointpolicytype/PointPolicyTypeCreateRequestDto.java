d72f4fcfaccd112ed6f94b3eb9e79dae30b2e874
hun_e <orutaku15@gmail.com>
2024-02-21 15:19:36 +0900

    refactor: DTO validation 메세지 추가

--- src/main/java/com/nhnacademy/inkbridge/backend/dto/pointpolicytype/PointPolicyTypeCreateRequestDto.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/dto/pointpolicytype/PointPolicyTypeCreateRequestDto.java
@@ -2,6 +2,7 @@ package com.nhnacademy.inkbridge.backend.dto.pointpolicytype;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,6 +22,7 @@ public class PointPolicyTypeCreateRequestDto {
     @NotBlank(message = "포인트 정책 유형은 필수 입력 항목입니다.")
     private String policyType;
 
+    @NotNull(message = "적립 포인트는 필수 입력 항목입니다.")
     @Min(value = 0, message = "적립 포인트는 음수 일수 없습니다.")
     private Long accumulatePoint;
 }
