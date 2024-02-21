d977e31abada74af1ab679bafaecd1343e5b36b5
hun_e <orutaku15@gmail.com>
2024-02-21 21:17:45 +0900

    refactor: 사용하지 않는 import 제거

--- src/test/java/com/nhnacademy/inkbridge/backend/controller/AccumulationRatePolicyControllerTest.java
+++ src/test/java/com/nhnacademy/inkbridge/backend/controller/AccumulationRatePolicyControllerTest.java
@@ -2,7 +2,6 @@ package com.nhnacademy.inkbridge.backend.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
