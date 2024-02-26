375e388872f12a6e7d0d54de678bb4cb19f25906
LeeJungBum <jungbum05114@gmail.com>
2024-02-26 10:44:25 +0900

    refactor: 쿠폰관련 javadoc

--- src/main/java/com/nhnacademy/inkbridge/backend/repository/CouponRepository.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/repository/CouponRepository.java
@@ -15,8 +15,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface CouponRepository extends JpaRepository<Coupon, String> {
 
+    /**
+     * 중복된 이름의 쿠폰이 존재하는지 찾는 메소드.
+     *
+     * @param name 이름
+     * @return 중복이면 true, 아니면 false
+     */
     boolean existsByCouponName(String name);
 
+    /**
+     * 쿠폰상태로 리스트를 페이지 갯수에맞게 찾아주는 메소드.
+     *
+     * @param couponStatus 쿠폰상태
+     * @param pageable     페이지, 사이즈
+     * @return 쿠폰상태를 기준으로 페이지리스트에 저장된 쿠폰들
+     */
     Page<CouponReadResponseDto> findByCouponStatus(CouponStatus couponStatus, Pageable pageable);
 
 }
