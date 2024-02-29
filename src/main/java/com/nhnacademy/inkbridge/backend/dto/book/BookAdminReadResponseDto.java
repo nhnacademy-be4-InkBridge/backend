3b6ed7eab50118bb4dd1fdec67e2ca040081a1b9
minm063 <jangmin063@gmail.com>
2024-02-29 16:41:57 +0900

    feat: dto 추가

--- src/main/java/com/nhnacademy/inkbridge/backend/dto/book/BookAdminReadResponseDto.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/dto/book/BookAdminReadResponseDto.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.inkbridge.backend.dto.book;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
+import com.nhnacademy.inkbridge.backend.dto.bookstatus.BookStatusReadResponseDto;
+import com.nhnacademy.inkbridge.backend.dto.category.ParentCategoryReadResponseDto;
+import com.nhnacademy.inkbridge.backend.dto.tag.TagReadResponseDto;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,59 +11,28 @@ import lombok.Getter;
  * class: BookAdminReadResponseDto.
  *
  * @author minm063
- * @version 2024/02/15
+ * @version 2024/02/29
  */
 @Getter
 public class BookAdminReadResponseDto {
 
-    private final String bookTitle;
-
-    private final String bookIndex;
-
-    private final String description;
-
-    private final LocalDate publicatedAt;
-
-    private final String isbn;
-
-    private final Long regularPrice;
-
-    private final Long price;
-
-    private final BigDecimal discountRatio;
-
-    private final Integer stock;
-
-    private final Boolean isPackagable;
-
-    private final Long authorId;
-
-    private final Long publisherId;
-
-    private final Long statusId;
-
-    private final String url;
-
+    List<ParentCategoryReadResponseDto> parentCategoryReadResponseDtoList;
+    List<PublisherReadResponseDto> publisherReadResponseDtoList;
+    List<AuthorReadResponseDto> authorReadResponseDtoList;
+    List<BookStatusReadResponseDto> bookStatusReadResponseDtoList;
+    List<TagReadResponseDto> tagReadResponseDtoList;
 
     @Builder
-    public BookAdminReadResponseDto(String bookTitle, String bookIndex, String description,
-        LocalDate publicatedAt, String isbn, Long regularPrice, Long price,
-        BigDecimal discountRatio,
-        Integer stock, Boolean isPackagable, Long authorId, Long publisherId, Long statusId,
-        String url) {
-        this.bookTitle = bookTitle;
-        this.bookIndex = bookIndex;
-        this.description = description;
-        this.publicatedAt = publicatedAt;
-        this.isbn = isbn;
-        this.regularPrice = regularPrice;
-        this.price = price;
-        this.discountRatio = discountRatio;
-        this.stock = stock;
-        this.isPackagable = isPackagable;
-        this.authorId = authorId;
-        this.publisherId = publisherId;
-        this.statusId = statusId;
-        this.url = url;
+    public BookAdminReadResponseDto(
+        List<ParentCategoryReadResponseDto> parentCategoryReadResponseDtoList,
+        List<PublisherReadResponseDto> publisherReadResponseDtoList,
+        List<AuthorReadResponseDto> authorReadResponseDtoList,
+        List<BookStatusReadResponseDto> bookStatusReadResponseDtoList,
+        List<TagReadResponseDto> tagReadResponseDtoList) {
+        this.parentCategoryReadResponseDtoList = parentCategoryReadResponseDtoList;
+        this.publisherReadResponseDtoList = publisherReadResponseDtoList;
+        this.authorReadResponseDtoList = authorReadResponseDtoList;
+        this.bookStatusReadResponseDtoList = bookStatusReadResponseDtoList;
+        this.tagReadResponseDtoList = tagReadResponseDtoList;
     }
 }
