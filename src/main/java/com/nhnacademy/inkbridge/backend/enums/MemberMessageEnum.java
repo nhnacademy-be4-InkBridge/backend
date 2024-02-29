ebd1d0c4d5211d3b4df93090194b7a8ba21c189f
hun_e <122001936+Huni0819@users.noreply.github.com>
2024-02-29 17:03:45 +0900

    refactor: 멤버 Enum 추가

--- src/main/java/com/nhnacademy/inkbridge/backend/enums/MemberMessageEnum.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/enums/MemberMessageEnum.java
@@ -8,6 +8,7 @@ package com.nhnacademy.inkbridge.backend.enums;
  */
 public enum MemberMessageEnum {
     MEMBER_NOT_FOUND("요청하신 맴버를 찾을 수 없습니다."),
+    MEMBER_ALREADY_EXIST("이미 존재하는 멤버입니다."),
     MEMBER_ID("MemberId: ");
 
     private final String message;
