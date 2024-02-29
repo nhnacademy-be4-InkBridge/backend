86bde3a147227db5a4bfcb299118306081715d09
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2024-02-29 14:03:07 +0900

    refactor: 팀 컨벤션에 맞춰서 코드 리팩토링

--- src/main/java/com/nhnacademy/inkbridge/backend/enums/CategoryMessageEnum.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/enums/CategoryMessageEnum.java
@@ -7,5 +7,18 @@ package com.nhnacademy.inkbridge.backend.enums;
  * @version 2/16/24
  */
 public enum CategoryMessageEnum {
-    CATEGORY_ALREADY_EXIST, CATEGORY_NOT_FOUND,SUB_CATEGORY_EXIST,CATEGORY_VALID_FAIL;
+    CATEGORY_NOT_FOUND("존재하지 않는 카테고리입니다."),
+
+    PARENT_CATEGORY_NOT_FOUND("존재하지 않는 부모 카테고리입니다."),
+    CATEGORY_VALID_FAIL("카테고리는 1~10글자여야 합니다.");
+
+    private String message;
+
+    CategoryMessageEnum(String message) {
+        this.message =message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
