6ac2cfad0262f1f7e585a878d5d7c53c4341f25c
minm063 <jangmin063@gmail.com>
2024-03-11 23:49:51 +0900

    Revert "feat: 장바구니 기능 개발(비회원)"
    This reverts commit ea5f36f0fc100e1016506629e1024b9ed5d38506.

--- src/main/java/com/nhnacademy/inkbridge/backend/repository/custom/BookRepositoryCustom.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/repository/custom/BookRepositoryCustom.java
@@ -4,10 +4,7 @@ import com.nhnacademy.inkbridge.backend.dto.book.BookAdminSelectedReadResponseDt
 import com.nhnacademy.inkbridge.backend.dto.book.BookReadResponseDto;
 import com.nhnacademy.inkbridge.backend.dto.book.BooksAdminReadResponseDto;
 import com.nhnacademy.inkbridge.backend.dto.book.BooksReadResponseDto;
-import com.nhnacademy.inkbridge.backend.dto.cart.CartReadResponseDto;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
@@ -63,5 +60,4 @@ public interface BookRepositoryCustom {
      */
     Optional<BookAdminSelectedReadResponseDto> findBookByAdminByBookId(Long bookId);
 
-    List<CartReadResponseDto> findByBookIdIn(Set<Long> bookIdList);
 }
