fa5c0ae3bea5ea8821f73004c5b3c7f117407569
minm063 <jangmin063@gmail.com>
2024-03-13 04:04:59 +0900

    feat: 장바구니 생성, 조회 api 추가

--- src/main/java/com/nhnacademy/inkbridge/backend/service/CartService.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/service/CartService.java
@@ -1,8 +1,8 @@
 package com.nhnacademy.inkbridge.backend.service;
 
+import com.nhnacademy.inkbridge.backend.dto.cart.CartCreateRequestDto;
 import com.nhnacademy.inkbridge.backend.dto.cart.CartReadResponseDto;
 import java.util.List;
-import java.util.Set;
 
 /**
  * class: CartService.
@@ -12,5 +12,19 @@ import java.util.Set;
  */
 public interface CartService {
 
-    List<CartReadResponseDto> getCart(Set<Long> bookIdList);
+    /**
+     * memberId에 따른 장바구니를 조회하는 메서드입니다.
+     *
+     * @param memberId Long
+     * @return CartReadResponseDto
+     */
+    List<CartReadResponseDto> getCart(Long memberId);
+
+    /**
+     * 장바구니를 생성하는 메서드입니다.
+     *
+     * @param cartList CartCreateRequestDto
+     */
+    void createCart(List<CartCreateRequestDto> cartList);
+
 }
