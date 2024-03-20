9d1ff3b3bbd856a17e0b8c0370db717e955199c7
정병훈 <rootofproblem23@gmail.com>
2024-03-20 17:20:55 +0900

    feat: 리뷰 파일 가져오기

--- src/main/java/com/nhnacademy/inkbridge/backend/service/FileService.java
+++ src/main/java/com/nhnacademy/inkbridge/backend/service/FileService.java
@@ -3,8 +3,6 @@ package com.nhnacademy.inkbridge.backend.service;
 
 import com.nhnacademy.inkbridge.backend.dto.file.FileCreateResponseDto;
 import com.nhnacademy.inkbridge.backend.entity.File;
-import java.util.List;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -43,6 +41,5 @@ public interface FileService {
      */
     ResponseEntity<byte[]> loadFileById(Long fileId);
 
-    Map<Long, List<String>> getAllFileByReviewId();
 }
 
