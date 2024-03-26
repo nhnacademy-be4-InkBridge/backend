db5ce0a66193aa1b5630b361d74db6784edce21b
nuheajiohc <113998025+nuheajiohc@users.noreply.github.com>
2024-03-26 21:31:09 +0900

    docs: 출판사, 카테고리 레스트독스 작성

--- src/test/java/com/nhnacademy/inkbridge/backend/controller/PublisherControllerTest.java
+++ src/test/java/com/nhnacademy/inkbridge/backend/controller/PublisherControllerTest.java
@@ -4,9 +4,16 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,11 +26,14 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -33,7 +43,7 @@ import org.springframework.test.web.servlet.MockMvc;
  * @author choijaehun
  * @version 2024/03/20
  */
-
+@AutoConfigureRestDocs
 @WebMvcTest(PublisherController.class)
 class PublisherControllerTest {
 
@@ -54,8 +64,10 @@ class PublisherControllerTest {
         mockMvc.perform(post("/api/admin/publishers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isCreated());
-
+            .andExpect(status().isCreated())
+            .andDo(document("publisher/create",
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름"))));
         verify(publisherService, times(1)).createPublisher(request);
     }
 
@@ -67,7 +79,10 @@ class PublisherControllerTest {
         mockMvc.perform(post("/api/admin/publishers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/create-validation",
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름"))));
 
         verify(publisherService, times(0)).createPublisher(request);
     }
@@ -79,7 +94,10 @@ class PublisherControllerTest {
         mockMvc.perform(post("/api/admin/publishers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/create-validation",
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름"))));
 
         verify(publisherService, times(0)).createPublisher(request);
     }
@@ -92,7 +110,10 @@ class PublisherControllerTest {
         mockMvc.perform(post("/api/admin/publishers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/create-validation",
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름"))));
 
         verify(publisherService, times(0)).createPublisher(request);
     }
@@ -107,7 +128,14 @@ class PublisherControllerTest {
 
         mockMvc.perform(get("/api/admin/publishers"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.content[0].publisherId").value(1L));
+            .andExpect(jsonPath("$.content[0].publisherId").value(1L))
+            .andDo(document("publisher/read",
+                relaxedResponseFields(
+                    fieldWithPath("content[]").description("Array of publishers"),
+                    fieldWithPath("content[].publisherId").description("출판사 아이디").type(
+                        JsonFieldType.NUMBER),
+                    fieldWithPath("content[].publisherName").description("출판사 이름").type(JsonFieldType.STRING))
+            ));
     }
 
     @Test
@@ -119,11 +147,19 @@ class PublisherControllerTest {
 
         ReflectionTestUtils.setField(request, "publisherName", publisherName);
 
-        mockMvc.perform(put("/api/admin/publishers/1")
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/admin/publishers/{publisherId}",1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andDo(document("publisher/update",
+                pathParameters(
+                    parameterWithName("publisherId").description("출판사 아이디")
+                ),
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름").type(JsonFieldType.STRING)
+                )
+            ));
 
         verify(publisherService, times(1)).updatePublisher(publisherId, request);
     }
@@ -134,10 +170,18 @@ class PublisherControllerTest {
         Long publisherId = 1L;
         PublisherUpdateRequestDto request = new PublisherUpdateRequestDto();
         ReflectionTestUtils.setField(request, "publisherName", "");
-        mockMvc.perform(put("/api/admin/publishers/1")
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/admin/publishers/{publisherId}",1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/update-validation",
+                pathParameters(
+                    parameterWithName("publisherId").description("출판사 아이디")
+                ),
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름").type(JsonFieldType.STRING)
+                )
+            ));
 
         verify(publisherService, times(0)).updatePublisher(publisherId, request);
     }
@@ -148,10 +192,18 @@ class PublisherControllerTest {
         Long publisherId = 1L;
         PublisherUpdateRequestDto request = new PublisherUpdateRequestDto();
         ReflectionTestUtils.setField(request, "publisherName", "1234567890123456789012345678901");
-        mockMvc.perform(put("/api/admin/publishers/1")
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/admin/publishers/{publisherId}",1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/update-validation",
+                pathParameters(
+                    parameterWithName("publisherId").description("출판사 아이디")
+                ),
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름").type(JsonFieldType.STRING)
+                )
+            ));
 
         verify(publisherService, times(0)).updatePublisher(publisherId, request);
     }
@@ -161,10 +213,18 @@ class PublisherControllerTest {
     void when_updatePublisher_expect_fail_3() throws Exception {
         Long publisherId = 1L;
         PublisherUpdateRequestDto request = new PublisherUpdateRequestDto();
-        mockMvc.perform(put("/api/admin/publishers/1")
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/admin/publishers/{publisherId}",1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-            .andExpect(status().isUnprocessableEntity());
+            .andExpect(status().isUnprocessableEntity())
+            .andDo(document("publisher/update-validation",
+                pathParameters(
+                    parameterWithName("publisherId").description("출판사 아이디")
+                ),
+                requestFields(
+                    fieldWithPath("publisherName").description("출판사 이름").type(JsonFieldType.NULL)
+                )
+            ));
 
         verify(publisherService, times(0)).updatePublisher(publisherId, request);
     }
